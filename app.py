92f6b3ef2e8f83813ab5ae2aa7a2cacd47e55485
ingveliz02ia <ingveliz02ia@gmail.com>
2024-11-20 19:34:28 -0500

    nombre separado

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, request, render_template, redirect, url_for,flash 
+from flask import Flask, request, render_template, redirect, url_for, flash
 import requests
 from waitress import serve
 
-
 app = Flask(__name__)
 app.secret_key = '4f3c2e1d9a8c7b6a5e4d3c2b1f0a9e8c7b6a5e4d3c2b1f0a9e8c7d6a5f4c3e2'
+
 # Ruta para mostrar el formulario
 @app.route('/')
 def form():
@@ -14,35 +14,36 @@ def form():
 # Ruta para procesar el formulario y enviar datos al webhook
 @app.route('/submit', methods=['POST'])
 def submit():
-    name = request.form['full_name']
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
     email = request.form['email_name']
     phone_number = request.form['phone_number']
 
     # Configura la URL del webhook de tu escenario de Make
-    
-    webhook_urlA = 'https://hook.eu2.make.com/dbxyau9e7ho5113994doa2acutm4n3be' #make
-    webhook_urlB = 'datosiniciales-production.up.railway.app/receive-data' #funciono
-    #webhook_urlA = 'https://2c46-209-45-58-245.ngrok-free.app/receive-data'# Quisimos agregarle alservidor no se logro
-        
+    webhook_urlA = 'https://hook.eu2.make.com/dbxyau9e7ho5113994doa2acutm4n3be' # make
+    webhook_urlB = 'https://datosiniciales-production.up.railway.app/receive-data' # funcionó
+    # webhook_urlA = 'https://2c46-209-45-58-245.ngrok-free.app/receive-data' # Quisimos agregarle al servidor no se logró
 
     # Datos a enviar al webhook
     data = {
-        'name': name,
+        'first_name': first_name,
+        'last_name': last_name,
         'email': email,
         'phone_number': phone_number
     }
+
     # Enviar datos al webhook usando una solicitud POST
-    response = requests.post(webhook_urlA, json=data)
-    response = requests.post(webhook_urlB, json=data)
-    if response.status_code == 200:
-        flash ('¡Mensaje enviado con éxito!', 'success')
+    responseA = requests.post(webhook_urlA, json=data)
+    responseB = requests.post(webhook_urlB, json=data)
+
+    if responseA.status_code == 200 and responseB.status_code == 200:
+        flash('¡Mensaje enviado con éxito!', 'success')
     else:
-        flash ('Error al enviar los datos', 'error')
+        flash('Error al enviar los datos', 'error')
 
     return redirect(url_for('form'))
 
-
-    
 if __name__ == '__main__':
-    #app.run(debug=True)
-    serve(app, host='0.0.0.0', port=80)
+    app.run(debug=True)
+    # serve(app, host='0.0.0.0', port=80)
+
